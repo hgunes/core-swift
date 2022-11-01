659e541ce23c8f42cccf5083da8c9d1a27bce008
hgunes <h.gunes13@gmail.com>
2022-11-01 18:32:41 +0100

    implement subject and custom string subscriber

--- CoreSwift/CoreSwift/Combine.playground/Contents.swift
+++ CoreSwift/CoreSwift/Combine.playground/Contents.swift
@@ -1,59 +1,102 @@
 
 
 //: # Combine
-//: ### Notification Center
+//: ## Basics
+//: ### Publisher & Subscriber
 import UIKit
 import Combine
 
-enum Status {
-    case failed
-    case passed
-}
+//enum Status {
+//    case failed
+//    case passed
+//}
+//
+//class Exam {
+//    var grade: Int
+//
+//    var status: Status {
+//
+//        if self.grade < 65 {
+//            return .failed
+//        } else {
+//            return .passed
+//        }
+//    }
+//
+//    init(grade: Int) {
+//        self.grade = grade
+//
+//        evaluateStatus()
+//    }
+//
+//    func evaluateStatus() {
+//        let notification = Notification.Name("Pass")
+//
+//
+//        let publisher = NotificationCenter.default.publisher(for: notification)
+//
+//        let subscriber = publisher.sink { _ in
+//            self.getRewarded()
+//        }
+//
+//        if self.status == .passed {
+//            NotificationCenter.default.post(name: notification, object: nil)
+//        } else {
+//            self.failed()
+//        }
+//    }
+//
+//    func getRewarded() {
+//        print("Congrats, you passed! I bought a car for you!")
+//    }
+//
+//    func failed() {
+//        print("You didn't study enough!")
+//    }
+//
+//}
+//
+//let exam = Exam(grade: 65)
 
-class Exam {
-    var grade: Int
-    
-    var status: Status {
-        
-        if self.grade < 65 {
-            return .failed
-        } else {
-            return .passed
-        }
-    }
-    
-    init(grade: Int) {
-        self.grade = grade
-        
-        evaluateStatus()
-    }
-    
-    func evaluateStatus() {
-        let notification = Notification.Name("Pass")
-        
-        
-        let publisher = NotificationCenter.default.publisher(for: notification)
-        
-        let subscriber = publisher.sink { _ in
-            self.getRewarded()
-        }
-        
-        if self.status == .passed {
-            NotificationCenter.default.post(name: notification, object: nil)
-        } else {
-            self.failed()
-        }
-    }
-    
-    func getRewarded() {
-        print("Congrats, you passed! I bought a car for you!")
-    }
-    
-    func failed() {
-        print("You didn't study enough!")
-    }
-    
-}
+//: ### Subjects
 
-let exam = Exam(grade: 65)
-
+//enum MyError: Error {
+//    case someError
+//}
+//
+//class StringSubscriber: Subscriber {        // custom subscriber
+//
+//    func receive(subscription: Subscription) {
+//        subscription.request(.max(2))
+//    }
+//
+//    func receive(_ input: String) -> Subscribers.Demand {
+//        print(input)
+//        return .max(1)
+//    }
+//
+//    func receive(completion: Subscribers.Completion<MyError>) {
+//        print("Completed")
+//    }
+//
+//    typealias Input = String
+//    typealias Failure = MyError
+//}
+//
+//let subscriber = StringSubscriber()
+//let subject = PassthroughSubject<String, MyError>()
+//
+//subject.subscribe(subscriber)
+//
+//let subscription = subject.sink { completion in
+//    print("Receibed Completion from sink")
+//} receiveValue: { value in
+//    print("Received value from sink")
+//}
+//
+//subject.send("A")
+//subscription.cancel()
+//
+//subject.send("B")
+//subject.send("C")
+//subject.send("D")
